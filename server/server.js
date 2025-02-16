1d10e10a3ac7787dce89cfdff1181505be0e7c56
Iheb Zarrad <126171975+ZARRADIHEB@users.noreply.github.com>
2025-02-16 18:59:59 +0100

    Revert CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,20 @@ import shopAddressRouter from "./routes/shop/address-routes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin:true,credentials:true}));
+app.use(
+cors({
+origin: process.env.FRONTEND_URL,
+methods: ["GET", "POST", "DELETE", "PUT"],
+allowedHeaders: [
+"Content-Type",
+"Authorization",
+"Cache-Control",
+"Expires",
+"Pragma",
+],
+credentials: true,
+})
+);
 
 app.use(cookieParser());
 app.use(express.json());
