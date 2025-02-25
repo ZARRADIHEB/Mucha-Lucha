9a1dea459f67c5762bd1561a57939e8d850b6d04
Iheb Zarrad <126171975+ZARRADIHEB@users.noreply.github.com>
2025-02-25 19:26:25 +0100

    debugging

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -141,9 +141,9 @@ const login = async (req, res) => {
 const logout = (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
+     httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     });
 
     return res.status(200).json({
